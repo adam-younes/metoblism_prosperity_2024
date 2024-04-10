7324d56861cbb0efce35406cc3d13e4668eebcfc
adam-younes <samakmashwee@gmail.com>
2024-04-10 09:46:57 -0500

    updated amethyst trading algo

--- trader.py
+++ trader.py
@@ -3,26 +3,70 @@ from datamodel import OrderDepth, UserId, TradingState, Order
 # import numpy
 # import statistics
 # import math
-# import typing
+import typing
+import collections
 # import jasonpickle
 import copy
 
-products = {
-    'AMETHYST': 0,
-    'STARFRUIT': 0
-}
 
 class Trader:
-    current_positions = copy.deepcopy(products)
 
-    def trade_amethysts(self, oder_depth, bid, ask):
-        # market make at sweet spot (9997.0)
-        # sell at sweet spot (10003.0)
-        return
+    position_limit = {
+        'AMETHYSTS': 20,
+        'STARFRUIT': 20
+    }
 
-    def trade_starfruit(self, order_depth, bit, ask):
+    position = {
+        'AMETHYSTS': 0,
+        'STARFRUIT': 0 
+    }
+
+    def unpack_orders(self, orders, intent=-1):
+        volume = 0
+        best_bid_ask = -1
+        max_volume = -1
+
+        for ask, vol in orders:
+            volume += vol
+            if volume > max_volume:
+                max_volume = volume
+                best_value = ask
+
+        return volume, best_bid_ask
+
+    def trade_amethysts(self, order_depth, max_bid, min_ask):
+        AME = 'AMETHYSTS'
+        orders: List[Order] = []
+        current_position = self.position[AME]
+
+        for current_ask, current_ask_amount in order_depth.sell_orders.items():
+            if current_ask <= max_bid and current_position < self.position_limit[AME]:
+                orders.append(Order(AME, current_ask, -current_ask_amount))
+
+        for current_bid, current_bid_amount in order_depth.buy_orders.items():
+            if current_bid >= min_ask and current_position > -self.position_limit[AME]:
+                orders.append(Order(AME, current_bid, -current_bid_amount))
+
+        return orders
+
+
+    def trade_starfruit(self, order_depth, max_bid, min_ask):
+        STA = 'STARFRUIT'
         # buy at lowball price every morning
         # sell at highball every afternoon
+        orders: List[Order] = []
+        
+        sell_orders = order_depth.sell_orders.items()
+        buy_orders = order_depth.buy_orders.items()
+
+        sell_volume, best_ask = self.unpack_orders(sell_orders)
+        buy_volume, best_bid = self.unpack_orders(buy_orders)
+
+        current_position = self.position[STA]
+
+        for ask, vol in sell_orders:
+            
+
         return
 
     def run(self, state: TradingState):
@@ -30,34 +74,17 @@ class Trader:
         # symbols as an input, and outputs a list of orders to be sent
         print("traderData: " + state.traderData)
         print("Observations: " + str(state.observations))
-        
-        result: Dict[str, List[Order]] = {}
 
-        for product in state.order_depths:
-            order_depth: OrderDepth = state.order_depths[product]
-            orders: List[Order] = []
-            acceptable_price = 10;  # Participant should calculate this value
-            print("Acceptable price : " + str(acceptable_price))
-            print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
-    
-            if len(order_depth.sell_orders) != 0:
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                if int(best_ask) < acceptable_price:
-                    print("BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_amount))
-    
-            if len(order_depth.buy_orders) != 0:
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                if int(best_bid) > acceptable_price:
-                    print("SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_amount))
-            
-            result[product] = orders
-    
-    
-        # String value holding Trader state data required. It will be delivered
+        # for key, val in state.position.items():
+        #    self.cur_equity[key] = val
+        
+        all_orders = { 'AMETHYSTS': [],
+                       'STARFRUIT': [] }
+
+        all_orders['AMETHYSTS'] = self.trade_amethysts(state.order_depths['AMETHYSTS'], 10000, 10000)
+
         # as TradingState.traderData on next execution.
         traderData = "SAMPLE" 
 
         conversions = 1
-        return result, conversions, traderData
+        return all_orders, conversions, traderData
