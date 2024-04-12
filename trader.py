407423b53137dce60e41258d65b1672794b493b6
adam-younes <samakmashwee@gmail.com>
2024-04-12 15:55:53 -0500

    Round 2 Data

--- trader.py
+++ trader.py
@@ -1,6 +1,6 @@
 from datamodel import OrderDepth, UserId, TradingState, Order
 # import pandas
-# import numpy
+import numpy as np
 # import statistics
 # import math
 import typing
@@ -8,6 +8,7 @@ import collections
 # import jasonpickle
 import copy
 
+INF = 10e9
 
 class Trader:
 
@@ -21,16 +22,25 @@ class Trader:
         'STARFRUIT': 0 
     }
 
-    def unpack_orders(self, orders, intent=-1):
+    product_cache = {
+        'AMETHYSTS': np.array([]),
+        'STARFRUIT': np.array([])
+    }
+
+    starfruit_count = 100
+
+    def unpack_orders(self, order_dict, buy=0):
         volume = 0
         best_bid_ask = -1
         max_volume = -1
 
-        for ask, vol in orders:
+        for ask, vol in order_dict:
+            if buy == 0:
+                vol *= -1
             volume += vol
             if volume > max_volume:
-                max_volume = volume
-                best_value = ask
+                max_volume = vol
+                best_bid_ask = ask
 
         return volume, best_bid_ask
 
@@ -42,38 +52,87 @@ class Trader:
         for current_ask, current_ask_amount in order_depth.sell_orders.items():
             if current_ask <= max_bid and current_position < self.position_limit[AME]:
                 orders.append(Order(AME, current_ask, -current_ask_amount))
+                current_position -= current_ask_amount
 
         for current_bid, current_bid_amount in order_depth.buy_orders.items():
             if current_bid >= min_ask and current_position > -self.position_limit[AME]:
                 orders.append(Order(AME, current_bid, -current_bid_amount))
+                current_position -= current_bid_amount
 
         return orders
 
+    def linear_regression(self, product):
+        A = np.vstack([np.ones(len(product)), np.arange(1, len(product) + 1)]).T
+        y = product[:, np.newaxis]
+
+        alpha = np.dot(np.dot(np.linalg.inv(np.dot(A.T, A)), A.T), y)
+
+        # Predicting the value for a given x
+        prediction = alpha[0] + alpha[1] * (len(product) + 1)
+
+        return prediction
 
     def trade_starfruit(self, order_depth, max_bid, min_ask):
         STA = 'STARFRUIT'
         # buy at lowball price every morning
         # sell at highball every afternoon
-        orders: List[Order] = []
-        
-        sell_orders = order_depth.sell_orders.items()
-        buy_orders = order_depth.buy_orders.items()
 
-        sell_volume, best_ask = self.unpack_orders(sell_orders)
-        buy_volume, best_bid = self.unpack_orders(buy_orders)
+        orders: List[Order] = []
 
         current_position = self.position[STA]
 
-        for ask, vol in sell_orders:
-            
+        sell_orders = order_depth.sell_orders.items()
+        buy_orders = order_depth.sell_orders.items()
 
-        return
+        for current_ask, current_ask_amount in sell_orders:
+            if current_ask <= max_bid and current_position < self.position_limit[STA]:
+                orders.append(Order(STA, current_ask, -current_ask_amount))
+                current_position -= current_ask_amount
+
+        for current_bid, current_bid_amount in sell_orders:
+            orders.append(Order(STA, current_bid, -current_bid_amount))
+            if current_bid >= min_ask and current_position > -self.position_limit[STA]:
+                orders.append(Order(STA, current_ask, -current_ask_amount))
+                current_position -= current_bid_amount
+
+       # sell_volume, best_ask = self.unpack_orders(sell_orders)
+       # buy_volume, best_bid = self.unpack_orders(buy_orders)
+
+       # better_ask = best_ask - 1
+       # better_bid = best_bid + 1
+
+       # final_bid = min(max_bid, better_bid)
+       # final_ask = max(min_ask, better_ask)
+
+       # for current_ask, current_ask_amount in sell_orders:
+       #     if current_ask <= max_bid or (self.position[STA] < 0 and current_ask == max_bid + 1) and current_position < self.position_limit[STA]:
+       #         order_volume = min(-current_ask_amount, self.position_limit[STA] - current_position)
+       #         current_position += order_volume
+       #         orders.append(Order(STA, current_ask, order_volume))
+
+       # if current_position < self.position_limit[STA]:
+       #     volume = self.position_limit[STA] - current_position
+       #     orders.append(Order(STA, final_bid, volume))
+       #     current_position += volume
+
+       # current_position = self.position_limit[STA]
+
+       # for current_bid, current_bid_amount in buy_orders:
+       #     if current_ask >= min_ask or (self.position[STA] > 0 and current_bid == min_ask + 1) and current_position > -self.position_limit[STA]:
+       #         order_volume = max(-current_bid_amount, -self.position_limit[STA] - current_position)
+       #         current_position += order_volume
+       #         orders.append(Order(STA, current_bid, order_volume))
+
+       # if current_position > -self.position_limit[STA]:
+       #     volume = -self.position_limit[STA] - current_position
+       #     orders.append(Order(STA, final_ask, volume))
+       #     current_position += volume
+
+        return orders
 
     def run(self, state: TradingState):
         # Only method required. It takes all buy and sell orders for all 
         # symbols as an input, and outputs a list of orders to be sent
-        print("traderData: " + state.traderData)
-        print("Observations: " + str(state.observations))
 
         # for key, val in state.position.items():
         #    self.cur_equity[key] = val
@@ -81,10 +140,27 @@ class Trader:
         all_orders = { 'AMETHYSTS': [],
                        'STARFRUIT': [] }
 
-        all_orders['AMETHYSTS'] = self.trade_amethysts(state.order_depths['AMETHYSTS'], 10000, 10000)
+        order_depths = state.order_depths
+
+        all_orders['AMETHYSTS'] += self.trade_amethysts(order_depths['AMETHYSTS'], 10000, 10000)
+
+        if len(self.product_cache['STARFRUIT']) == self.starfruit_count:
+            np.delete(self.product_cache['STARFRUIT'], 0)
+                
+        _, best_sta_bid = self.unpack_orders(order_depths['STARFRUIT'].buy_orders.items(), 1)
+        _, best_sta_ask = self.unpack_orders(order_depths['STARFRUIT'].sell_orders.items())
+
+        self.product_cache['STARFRUIT'] = np.append(self.product_cache['STARFRUIT'], [(best_sta_bid + best_sta_ask) / 2])
+
+        if len(self.product_cache['STARFRUIT']) >= self.starfruit_count:
+            starfruit_next = self.linear_regression(self.product_cache['STARFRUIT'])
+            # print("PREDICTION: " + str(starfruit_next))
+            # print("BUY_ORDERS: " + str(order_depths['STARFRUIT'].buy_orders))
+            # print("SELL_ORDERS: " + str(order_depths['STARFRUIT'].sell_orders))
+            all_orders['STARFRUIT'] += self.trade_starfruit(order_depths['STARFRUIT'], starfruit_next, starfruit_next)
 
         # as TradingState.traderData on next execution.
-        traderData = "SAMPLE" 
+        traderData = "SAMPLE"
 
         conversions = 1
         return all_orders, conversions, traderData
